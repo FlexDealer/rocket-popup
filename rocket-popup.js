eb657e659f2a389b2249a06611f7ec7bbb33988f
Nolan Neustaeter <nolan@flexdealer.com>
2014-08-26 11:52:43 -0700

    fix vertical overflow of iframes

--- rocket-popup.js
+++ rocket-popup.js
@@ -19,6 +19,7 @@ $(document).ready(function() {
 
 
 	$('.rkt-launcher').click(function(e) {
+		e.preventDefault();
 		e.stopPropagation();
 		$('.rkt-atmosphere').addClass('rkt-overcast');
 		var launcher = $(this),
@@ -28,11 +29,16 @@ $(document).ready(function() {
 		shuttle.attr('data-landing-sequence', '');
 		shuttle.attr('class', '');
 
+		var paddingX = Number(shuttle.css('padding-left').replace(/[^-\d\.]/g, '')) + Number(shuttle.css('padding-right').replace(/[^-\d\.]/g, '')),
+			paddingY = Number(shuttle.css('padding-top').replace(/[^-\d\.]/g, '')) + Number(shuttle.css('padding-bottom').replace(/[^-\d\.]/g, ''));
+
+		console.log(paddingX, paddingY);
+
 		if (launcher.data('width')) {
-			shuttle.css('width', launcher.data('width') + (shuttle.innerWidth() - shuttle.width()));
+			shuttle.css('width', Number(launcher.data('width')) + paddingX);
 		}
 		if (launcher.data('height')) {
-			shuttle.css('height', launcher.data('height') + (shuttle.innerHeight() - shuttle.height()));
+			shuttle.css('height', Number(launcher.data('height')) + paddingY);
 		}
 
 
@@ -41,12 +47,12 @@ $(document).ready(function() {
 		} else if (launcher.attr('rel') == 'image') {
 			shuttle.html('<img style="width:100%; height:auto;" src="' + launcher.attr('href') + '">' + '<div class="rkt-x">X</div>');
 		} else {
-			shuttle.html('<iframe style="width:100%; height:100%;" src="' + launcher.attr('href') + '"></iframe>' + '<div class="rkt-x">X</div>');
+			shuttle.html('<iframe style="width:100%; height:100%; height:calc(100% - ' + paddingY + 'px);" src="' + launcher.attr('href') + '"></iframe>' + '<div class="rkt-x">X</div>');
 			if (!launcher.data('width')) {
-				shuttle.css('width', 800 + (shuttle.innerWidth() - shuttle.width()));
+				shuttle.css('width', 800 + paddingX);
 			}
 			if (!launcher.data('height')) {
-				shuttle.css('height', 550 + (shuttle.innerHeight() - shuttle.height()));
+				shuttle.css('height', 600 + paddingY);
 			}
 		}
 
