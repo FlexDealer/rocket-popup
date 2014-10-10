6f5efb655698e986ce1f886891369851bf3c4e25
Nolan Neustaeter <nolan@flexdealer.com>
2014-10-09 19:17:15 -0700

    bug fixes

--- rocket-popup.js
+++ rocket-popup.js
@@ -1,71 +1,94 @@
 $(document).ready(function() {
 	$('body').append('<div id="rkt-shuttle"></div>');
 	
+
+
 	$('body').on('touchstart click', "div", function(e) {
-		if ($(this).hasClass('rkt-overcast') || (($(this).hasClass('rkt-x')))) {
+		if (($(this).hasClass('rkt-overcast') && !($('#rkt-shuttle').hasClass('rkt-permanent'))) || (($(this).hasClass('rkt-x')))) {
 			$('#rkt-shuttle').removeClass('rkt-orbiting');
 			setTimeout(landingSequence, 500);
 		}
 	});
 
-	function landingSequence() {
-		var shuttle = $('#rkt-shuttle');
-		if (shuttle.data('landing-sequence')) {
-			eval(shuttle.data('landing-sequence'));
-		}
-		$('.rkt-atmosphere').removeClass('rkt-overcast');
-		shuttle.html('');
-	}
 
 
 	$('.rkt-launcher').click(function(e) {
 		e.preventDefault();
 		e.stopPropagation();
-		$('.rkt-atmosphere').addClass('rkt-overcast');
-		var launcher = $(this),
-			shuttle = $('#rkt-shuttle');
 
-		shuttle.attr('style', '');
-		shuttle.attr('data-landing-sequence', '');
-		shuttle.attr('class', '');
-
-		var paddingX = Number(shuttle.css('padding-left').replace(/[^-\d\.]/g, '')) + Number(shuttle.css('padding-right').replace(/[^-\d\.]/g, '')),
-			paddingY = Number(shuttle.css('padding-top').replace(/[^-\d\.]/g, '')) + Number(shuttle.css('padding-bottom').replace(/[^-\d\.]/g, ''));
-
-		if (launcher.data('width')) {
-			shuttle.css('width', Number(launcher.data('width')) + paddingX);
+		var launcher = $(this);
+		
+		if ((launcher.data('disable-below')) && $(window).width() < launcher.data('disable-below')) {
+			return false;
 		}
-		if (launcher.data('height')) {
-			shuttle.css('height', Number(launcher.data('height')) + paddingY);
-		}
-
-
-		if (launcher.data('cargo')) {
-			shuttle.html(launcher.data('cargo') + '<div class="rkt-x">X</div>');
-		} else if (launcher.attr('rel') == 'image') {
-			shuttle.html('<img style="width:100%; height:auto;" src="' + launcher.attr('href') + '">' + '<div class="rkt-x">X</div>');
-		} else {
-			shuttle.html('<iframe style="width:100%; height:100%; height:calc(100% - ' + paddingY + 'px);" src="' + launcher.attr('href') + '"></iframe>' + '<div class="rkt-x">X</div>');
-			if (!launcher.data('width')) {
-				shuttle.css('width', 800 + paddingX);
-			}
-			if (!launcher.data('height')) {
-				shuttle.css('height', 600 + paddingY);
-			}
-		}
-
-
-		if (launcher.data('flight-plan')) {
-			eval(launcher.data('flight-plan'));
-		}
-		if (launcher.data('landing-sequence')) {
-			shuttle.attr('data-landing-sequence', launcher.data('landing-sequence'));
-		}
-		if (launcher.data('class')) {
-			shuttle.addClass(launcher.data('class'));
-		}
-
-		shuttle.addClass('rkt-orbiting');
+		
+		launch({
+			url:             launcher.attr('href'),
+			flightPlan:      launcher.data('flight-plan'),
+			landingSequence: launcher.data('landing-sequence'),
+			classes:         launcher.data('class'),
+			w:               launcher.data('width'),
+			h:               launcher.data('height'),
+			mode:            launcher.attr('rel'),
+			cargo:           launcher.data('cargo')
+		});
 		return false;
 	});
 });
+
+function launch(settings) {
+	$('.rkt-atmosphere').addClass('rkt-overcast');
+	var shuttle = $('#rkt-shuttle');
+
+	shuttle.attr('style', '');
+	shuttle.attr('data-landing-sequence', '');
+	shuttle.attr('class', '');
+
+	if (settings.classes) {
+		shuttle.addClass(settings.classes);
+	}
+	if (settings.landingSequence) {
+		shuttle.attr('data-landing-sequence', settings.landingSequence);
+	}
+
+	var paddingX = Number(shuttle.css('padding-left').replace(/[^-\d\.]/g, '')) + Number(shuttle.css('padding-right').replace(/[^-\d\.]/g, '')),
+		paddingY = Number(shuttle.css('padding-top').replace(/[^-\d\.]/g, '')) + Number(shuttle.css('padding-bottom').replace(/[^-\d\.]/g, ''));
+
+	if (settings.w) {
+		shuttle.css('width', Number(settings.w) + paddingX);
+	}
+	if (settings.h) {
+		shuttle.css('height', Number(settings.h) + paddingY);
+	}
+
+
+	if (settings.cargo) {
+		shuttle.html(settings.cargo + '<div class="rkt-x">X</div>');
+	} else if (settings.mode == 'image') {
+		shuttle.html('<img style="width:100%; height:auto;" src="' + settings.url + '">' + '<div class="rkt-x">X</div>');
+	} else {
+		shuttle.html('<iframe style="width:100%; height:100%; height:calc(100% - ' + paddingY + 'px);" src="' + settings.url + '"></iframe>' + '<div class="rkt-x">X</div>');
+		if (!settings.w) {
+			shuttle.css('width', 800 + paddingX);
+		}
+		if (!settings.h) {
+			shuttle.css('height', 600 + paddingY);
+		}
+	}
+
+	if (settings.flightPlan) {
+		eval(settings.flightPlan);
+	}
+
+	shuttle.addClass('rkt-orbiting');
+}
+
+
+function landingSequence() {
+	var shuttle = $('#rkt-shuttle');
+	if (shuttle.data('landing-sequence')) {
+		eval(shuttle.data('landing-sequence'));
+	}
+	$('.rkt-atmosphere').removeClass('rkt-overcast');
+	shuttle.html('');
+}
